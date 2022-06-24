2fa026de5e8f5d554d7e9da26507110d3be1e11c
Marcus Davanco <md.daves@gmail.com>
2022-06-24 07:10:58 -0300

    feat: Estilos globais do app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
-import styles from "../styles/home.module.scss";
 import Head from "next/head";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello World</h1>
+      <h1>Hello World</h1>
     </>
   );
 }
